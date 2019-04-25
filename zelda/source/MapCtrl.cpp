6d1a55bc8c5aad6a6c44595e48cd2dcd9f4803d9
mhigg <tekito@email>
2019-04-25 16:05:04 +0900

    add method

--- zelda/source/MapCtrl.cpp
+++ zelda/source/MapCtrl.cpp
@@ -21,6 +21,11 @@ bool MapCtrl::SetMapData(VECTOR2 mapPos, MAP_ID id)
 	return false;
 }
 
+MAP_ID MapCtrl::GetMapData(VECTOR2 mapPos)
+{
+	return MAP_ID::NONE/*stageMap[mapPos.x][mapPos.y]*/;
+}
+
 const VECTOR2 & MapCtrl::GetChipSize(void)
 {
 	return chipSize;
--- zelda/source/MapCtrl.h
+++ zelda/source/MapCtrl.h
@@ -1,6 +1,7 @@
 // ﾏｯﾌﾟID関連で、ﾏｯﾌﾟ上の情報を操作するｸﾗｽ
 
 #pragma once
+#include <vector>
 #include "VECTOR2.h"
 #include "MAP_ID.h"
 
@@ -17,11 +18,13 @@ public:
 	MapCtrl();
 	~MapCtrl();
 
-	bool SetUp(VECTOR2 stageSize, VECTOR2 chipSize);
-	bool SetMapData(VECTOR2 mapPos, MAP_ID id);
+	bool SetUp(VECTOR2 stageSize, VECTOR2 chipSize);	// ﾏｯﾌﾟﾃﾞｰﾀのﾘｻｲｽﾞ・初期化
+	bool SetMapData(VECTOR2 mapPos, MAP_ID id);			// ﾏｯﾌﾟIDの設定(大体ｴﾃﾞｨｯﾄ用？)
+	MAP_ID GetMapData(VECTOR2 mapPos);					// 指定したﾏｽのﾏｯﾌﾟIDを取得
 
 	const VECTOR2& GetChipSize(void);						// ChipSizeの取得
 
 private:
+	std::vector<MAP_ID> stageMap;  // ｽﾃｰｼﾞﾏｯﾌﾟﾃﾞｰﾀ
 	VECTOR2 chipSize;	// ゲームサイズ
 };
