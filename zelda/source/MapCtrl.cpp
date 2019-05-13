92679834a3c21e9bcf9aed9547ae80790aec7383
kuro <kc8520993@gmail.com>
2019-05-13 12:13:05 +0900

    map

--- zelda/source/MapCtrl.cpp
+++ zelda/source/MapCtrl.cpp
@@ -249,6 +249,8 @@ bool MapCtrl::SetUpGameObj(sharedListObj objList, bool modeFlag)
 			case MAP_ID::WALL34:
 			case MAP_ID::WALL35:
 			case MAP_ID::WALL36:
+			case MAP_ID::WALL37:
+			case MAP_ID::WALL38:
 			case MAP_ID::DOOR1:
 			case MAP_ID::DOOR2:
 			case MAP_ID::DOOR3:
@@ -355,6 +357,8 @@ void MapCtrl::Draw(bool flag)
 			case MAP_ID::WALL34:
 			case MAP_ID::WALL35:
 			case MAP_ID::WALL36:
+			case MAP_ID::WALL37:
+			case MAP_ID::WALL38:
 			case MAP_ID::DOOR1:
 			case MAP_ID::DOOR2:
 			case MAP_ID::DOOR3:
