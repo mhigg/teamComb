e1783772007a8a0e73c3b7d4f5ab81e90fd7dd59
kuro <kc8520993@gmail.com>
2019-05-22 12:33:38 +0900

    select

--- zelda/source/SelectScene.h
+++ zelda/source/SelectScene.h
@@ -3,7 +3,8 @@
 
 constexpr auto CHARA_NUM = 4;
 
-using SELECT_CHARA = std::array< bool, CHARA_NUM >;
+using SELECT_CHARA = std::array< int, CHARA_NUM >;
+using SELECT_POS = std::array< VECTOR2, CHARA_NUM >;
 
 class SelectScene :
 	public BaseScene
@@ -19,7 +20,7 @@ private:
 	void SelectDraw(void);
 	int Init(void);
 
-	SELECT_CHARA selectChara;
-	int nowChara;	// 今選択しているキャラ
+	SELECT_CHARA selectChara;	// プレイヤー毎の今選択しているキャラ
+	SELECT_POS selectPos;		// プレイヤー毎の立ち絵位置
 };
 
