eebd8e4e6d6e9ec2789c2084b1ebc09d56e44bee
Daichi <aaaaaaaaaaa>
2019-05-24 14:10:00 +0900

    a

--- zelda/source/Obj.cpp
+++ zelda/source/Obj.cpp
@@ -90,9 +90,9 @@ void Obj::Draw(void)
 		}
 	}
 	SetFontSize(18);
-	DrawFormatString(pos.x - scrollOffset.x + drawOffset.x, pos.y - scrollOffset.y + drawOffset.y,
+	//DrawFormatString(pos.x - scrollOffset.x + drawOffset.x, pos.y - scrollOffset.y + drawOffset.y,
 		0x00ffffff, "DIR:%d\n", static_cast<int>(dir));
-	//DrawFormatString(pos.x - scrollOffset.x +drawOffset.x , pos.y - scrollOffset.y +drawOffset.y,0x00ffffff, "scrX:%d\nscrY:%d", pos.x, pos.y);
+	DrawFormatString(pos.x - scrollOffset.x +drawOffset.x , pos.y - scrollOffset.y +drawOffset.y,0x00ffffff, "scrX:%d\nscrY:%d", pos.x, pos.y);
 	DrawCircle(pos.x - scrollOffset.x +drawOffset.x, pos.y - scrollOffset.y +drawOffset.y, hitRad.x, 0x00ffffff, false);
 }
 
