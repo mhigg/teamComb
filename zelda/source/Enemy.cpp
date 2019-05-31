1ee690e52a0cee82b18e41420f04ab500c3daf0d
Daichi <aaaaaaaaaaa>
2019-05-31 12:14:29 +0900

    Enemy 動き

--- zelda/source/Enemy.cpp
+++ zelda/source/Enemy.cpp
@@ -37,7 +37,13 @@ Enemy::Enemy(int enemyNum, VECTOR2 setUpPos, VECTOR2 drawOffset, int enCnt) :Obj
 		DIR_DOWN	,DIR_LEFT		,DIR_RIGHT,
 		DIR_LEFT		,DIR_DOWN	,DIR_UP,
 		DIR_RIGHT	,DIR_DOWN	,DIR_UP,
-		DIR_UP			,DIR_RIGHT	,DIR_LEFT
+		DIR_UP			,DIR_LEFT		,DIR_RIGHT
+	};
+	rootPass = {
+		DIR_UP			,DIR_RIGHT	,DIR_LEFT,
+		DIR_RIGHT	,DIR_UP		,DIR_DOWN,
+		DIR_LEFT		,DIR_UP		,DIR_DOWN,
+		DIR_DOWN	,DIR_RIGHT	,DIR_LEFT		
 	};
 	mapMoveTbl = {
 					true,			// NONE
@@ -136,19 +142,20 @@ Enemy::Enemy(int enemyNum, VECTOR2 setUpPos, VECTOR2 drawOffset, int enCnt) :Obj
 
 void Enemy::EnInit(void)
 {
+	nearP = 0;
 	speed = ENEMY_SPEED;
 	// ｶｳﾝﾄ系
 	deathCnt = 60;
 	timeCnt = 0;
 	actCnt = 60;
-	faintCnt = 0;
 	addCnt = { 0,0 };
 	oppFlag = false;
+	rootPos = VECTOR2(-1, -1);
+	firstPos = VECTOR2(-1, -1);
 }
 
 void Enemy::CheckFree(void)
 {
-	nearP = 0;
 	movePos = 0;
 	int actRoot = 0;
 	for (int i = 0; i < 4; i++)
@@ -160,9 +167,21 @@ void Enemy::CheckFree(void)
 			actNum[m] = 0;
 			passFlag[i + m * 4] = false;
 			checkPos[i + m * 4] = VECTOR2(0, 0);
-			comPos[i + m * 4] = 10000;
+			comPos[i + m * 4] = 10000.0;
 		}
 	}
+}
+
+bool Enemy::initAnim(void)
+{
+	int num = static_cast<int>(name) * 2;
+	AddAnim("右待機", num, 0, 2, 10, true);				// 右
+	AddAnim("左待機", num + 1, 0, 2, 10, true);		// 左
+	AddAnim("右探索", num, 2, 2, 10, true);
+	AddAnim("左探索", num + 1, 2, 2, 10, true);
+	AddAnim("右発見", num, 4, 1, 30, false);
+	AddAnim("左発見", num + 1, 4, 1, 30, false);
+	return true;
 }
 
 Enemy::~Enemy()
@@ -194,50 +213,57 @@ VECTOR2 Enemy::sidePos(VECTOR2 pos, DIR dir, int speed, int sideNum)
 
 VECTOR2 Enemy::Distance(DIR tmpDir,VECTOR2 pos)
 {
-	while (mapMoveTbl[static_cast<int>(lpMapCtrl.GetMapData(sidePos(pos, tmpDir, SpeedTbl[tmpDir][0], -hitRad.x)))]
-		&& mapMoveTbl[static_cast<int>(lpMapCtrl.GetMapData(sidePos(pos, tmpDir, SpeedTbl[tmpDir][0], hitRad.x - 1)))]
-		&& mapMoveTbl[static_cast<int>(lpMapCtrl.GetMapData(sidePos(pos, tmpDir, SpeedTbl[tmpDir][0], 0)))])
+	switch (tmpDir)
 	{
-		if (tmpDir == DIR_DOWN || tmpDir == DIR_UP)
-		{
-			pos.y += SpeedTbl[tmpDir][0];
-			if ((*PosTbl[tmpDir][TBL_MAIN]) == plPos[nearP].x)
-			{
-				return pos;
-			}
+	case DIR_DOWN:
+	case DIR_UP:
+		while (mapMoveTbl[static_cast<int>(lpMapCtrl.GetMapData(sidePos(pos, tmpDir, SpeedTbl[tmpDir][0], -hitRad.x)))]
+			&& mapMoveTbl[static_cast<int>(lpMapCtrl.GetMapData(sidePos(pos, tmpDir, SpeedTbl[tmpDir][0], hitRad.x - 1)))]
+			&& mapMoveTbl[static_cast<int>(lpMapCtrl.GetMapData(sidePos(pos, tmpDir, SpeedTbl[tmpDir][0], 0)))]
+			&& (pos.y != plPos[nearP].y))
+		{	
+			pos.y += SpeedTbl[tmpDir][0];	
 		}
-		else
+		break;
+	case DIR_RIGHT:
+	case DIR_LEFT:
+		while (mapMoveTbl[static_cast<int>(lpMapCtrl.GetMapData(sidePos(pos, tmpDir, SpeedTbl[tmpDir][0], -hitRad.x)))]
+			&& mapMoveTbl[static_cast<int>(lpMapCtrl.GetMapData(sidePos(pos, tmpDir, SpeedTbl[tmpDir][0], hitRad.x - 1)))]
+			&& mapMoveTbl[static_cast<int>(lpMapCtrl.GetMapData(sidePos(pos, tmpDir, SpeedTbl[tmpDir][0], 0)))]
+			&& (pos.x != plPos[nearP].x))
 		{
 			pos.x += SpeedTbl[tmpDir][0];
-			if ((*PosTbl[tmpDir][TBL_MAIN]) == plPos[nearP].y)
-			{
-				return pos;
-			}
 		}
-		
+		break;
+	default:
+		break;
 	}
 	return pos;
 }
 
-int Enemy::SerchPlayer(void)
+int Enemy::SerchPlayer(bool flag)
 {
 	VECTOR2 tmpPos = Enemy::pos;
 	for (int i = 0; i < 4; i++)
 	{
 		plPos[i] = lpInfoCtrl.GetPlayerPos(i);
 		if (plPos[i] != VECTOR2(-1, -1))
-		{		
+		{
 			VECTOR2 tmp = tmpPos - plPos[i];
 			enPos[i] = static_cast<int>(sqrt(tmp.x * tmp.x) + sqrt(tmp.y * tmp.y));
 		}
 	}
 	int plNum = 0;
 	for (int i = 1; i < 4; i++)
-	{	
+	{
 		if (enPos[i] < enPos[plNum] && enPos[i] != -1)
 		{
 			plNum = i;
-		}	
+		}
+	}
+	if (!flag)
+	{
+		return enPos[plNum];
 	}
 	return plNum;
 }
@@ -245,7 +271,6 @@ int Enemy::SerchPlayer(void)
 void Enemy::SetMove(const GameCtrl & controller, weakListObj objList)
 {
 	scrollOffset = lpInfoCtrl.GetAddScroll(0);
-
 	// 死んでるかどうか
 	if (lpInfoCtrl.GetEnemyFlag(Enemy::enCnt))
 	{
@@ -257,27 +282,26 @@ void Enemy::SetMove(const GameCtrl & controller, weakListObj objList)
 		deathCnt--;
 		if (deathCnt == 0)
 		{
-			lpInfoCtrl.SetEnemyPos(VECTOR2(-1, -1), enCnt);
 			deathFlag = lpInfoCtrl.GetEnemyFlag(Enemy::enCnt);
 		}
 		return;
 	}
 	// 最も近いﾌﾟﾚｲﾔｰの検索
-	nearP = SerchPlayer();
-	// ﾌﾟﾚｲﾔｰとの距離が画面内なら処理を行う
-	switch (action)
+	nearP = SerchPlayer(true);
+
+	// 敵の種類によってﾊﾟﾀｰﾝを変える
+	switch (name)
 	{
-	case ENEM_ACT::SERCH:
-		_updater = &Enemy::Serch;
+	case ENEMY::ENEMY_1:
+		GateKeeper();
 		break;
-	case ENEM_ACT::MOVE:
-		_updater = &Enemy::Move;
+	case ENEMY::ENEMY_2:
+		Undette();
 		break;
-	case ENEM_ACT::TRA:
-		_updater = &Enemy::Track;
+	case ENEMY::ENEMY_3:
+		Zombie();
 		break;
 	default:
-		_updater = &Enemy::Serch;
 		break;
 	}
 	(this->*_updater)(controller);
@@ -322,6 +346,19 @@ void Enemy::SetMove(const GameCtrl & controller, weakListObj objList)
 			break;
 		}
 		break;
+	case ENEM_ACT::CONST_MOVE:
+		switch (dir)
+		{
+		case DIR_RIGHT:
+			SetAnim("右発見");
+			break;
+		case DIR_LEFT:
+			SetAnim("左発見");
+			break;
+		default:
+			break;
+		}
+		break;
 	default:
 		break;
 	}
@@ -335,22 +372,102 @@ void Enemy::SetMove(const GameCtrl & controller, weakListObj objList)
 	behaviorCnt++;
 }
 
-bool Enemy::initAnim(void)
+void Enemy::GateKeeper(void)
 {
-	int num = static_cast<int>(name) * 2;
-	AddAnim("右待機", num, 0, 2, 10, true);				// 右
-	AddAnim("左待機", num + 1, 0, 2, 10, true);		// 左
-	AddAnim("右探索", num, 2, 2, 10, true);
-	AddAnim("左探索", num + 1, 2, 2, 10, true);
-	AddAnim("右発見", num, 4, 1, 30, false);
-	AddAnim("左発見", num + 1, 4, 1, 30, false);
-	return true;
+	switch (action)
+	{
+	case ENEM_ACT::WAIT:
+		if (GetRand(60) % 30 == 0)
+		{
+			action = ENEM_ACT::CONST_MOVE;
+		}
+		_updater = &Enemy::Wait;
+		break;
+	case ENEM_ACT::CONST_MOVE:
+		_updater = &Enemy::ConstMove;
+		break;
+	case ENEM_ACT::TRA:
+		_updater = &Enemy::Track;
+		break;
+	case ENEM_ACT::BACK:
+		_updater = &Enemy::Back;
+		break;
+	case ENEM_ACT::SERCH:
+		_updater = &Enemy::Serch;
+		break;
+	default:
+		action = ENEM_ACT::WAIT;
+		_updater = &Enemy::Wait;
+		break;
+	}
+	return;
+}
+
+void Enemy::Undette(void)
+{
+	switch (action)
+	{
+	case ENEM_ACT::MOVE:
+		_updater = &Enemy::Move;
+		break;
+	case ENEM_ACT::TRA:
+		_updater = &Enemy::Track;
+		break;
+	default:
+		action = ENEM_ACT::MOVE;
+		_updater = &Enemy::Move;
+		break;
+	}
+}
+
+void Enemy::Zombie(void)
+{
+}
+
+void Enemy::ConstMove(const GameCtrl & controller)
+{
+	if (SerchPlayer(false) < SERCH_LIM - 50)
+	{
+		firstPos = pos;
+		action = ENEM_ACT::SERCH;
+		return;
+	}
+	if (oppFlag)
+	{
+		Enemy::dir = DIR_RIGHT;
+		if (!mapMoveTbl[static_cast<int>(lpMapCtrl.GetMapData(sidePos(pos, Enemy::dir, SpeedTbl[Enemy::dir][0], -hitRad.x)))]
+			|| !mapMoveTbl[static_cast<int>(lpMapCtrl.GetMapData(sidePos(pos, Enemy::dir, SpeedTbl[Enemy::dir][0], hitRad.x - 1)))]
+			|| !mapMoveTbl[static_cast<int>(lpMapCtrl.GetMapData(sidePos(pos, Enemy::dir, SpeedTbl[Enemy::dir][0], 0)))])
+		{
+			// 移動不可のオブジェクトが隣にあった場合
+			oppFlag = false;
+			action = ENEM_ACT::WAIT;
+			return;
+		}
+		// 移動処理-----------------------------
+		(*PosTbl[Enemy::dir][TBL_MAIN]) += SpeedTbl[Enemy::dir][0];
+		return;
+	}
+	else
+	{
+		Enemy::dir = DIR_LEFT;
+		if (!mapMoveTbl[static_cast<int>(lpMapCtrl.GetMapData(sidePos(pos, Enemy::dir, SpeedTbl[Enemy::dir][0], -hitRad.x)))]
+			|| !mapMoveTbl[static_cast<int>(lpMapCtrl.GetMapData(sidePos(pos, Enemy::dir, SpeedTbl[Enemy::dir][0], hitRad.x - 1)))]
+			|| !mapMoveTbl[static_cast<int>(lpMapCtrl.GetMapData(sidePos(pos, Enemy::dir, SpeedTbl[Enemy::dir][0], 0)))])
+		{
+			// 移動不可のオブジェクトが隣にあった場合
+			oppFlag = true;
+			action = ENEM_ACT::WAIT;
+			return;
+		}
+		// 移動処理-----------------------------
+		(*PosTbl[Enemy::dir][TBL_MAIN]) += SpeedTbl[Enemy::dir][0];
+		return;
+	}
 }
 
 void Enemy::Move(const GameCtrl & controller)
 {
-	auto &chipSize = lpStageMng.GetChipSize().x;
-
 	int num = GetRand(10) + 20;
 	if (!(timeCnt % num))
 	{
@@ -394,16 +511,15 @@ void Enemy::Move(const GameCtrl & controller)
 
 void Enemy::Track(const GameCtrl & controller)
 {
-	auto &chipSize = lpStageMng.GetChipSize().x;
 	//-----------移動-------------
-
 	int tmpAct = actRoot % 4;
 	movePos = 0;
 	switch (tmpAct)
 	{
 	case 0:
 	case 3:
-		if ((*PosTbl[tmpAct][TBL_MAIN]) != checkPos[actRoot].y)
+		if ((*PosTbl[tmpAct][TBL_MAIN]) != checkPos[actRoot].y
+			&& ((*PosTbl[tmpAct][TBL_MAIN]) != plPos[nearP].y))
 		{
 			if (mapMoveTbl[static_cast<int>(lpMapCtrl.GetMapData(sidePos(pos, static_cast<DIR>(tmpAct), SpeedTbl[static_cast<DIR>(tmpAct)][0], -hitRad.x)))]
 				&& mapMoveTbl[static_cast<int>(lpMapCtrl.GetMapData(sidePos(pos, static_cast<DIR>(tmpAct), SpeedTbl[static_cast<DIR>(tmpAct)][0], hitRad.x - 1)))]
@@ -412,6 +528,12 @@ void Enemy::Track(const GameCtrl & controller)
 				dir = static_cast<DIR>(tmpAct);
 				(*PosTbl[tmpAct][TBL_MAIN]) += SpeedTbl[tmpAct][0];
 				movePos += SpeedTbl[tmpAct][0];
+				if ((*PosTbl[tmpAct][TBL_MAIN]) == plPos[nearP].y
+				|| (*PosTbl[tmpAct][TBL_MAIN]) == checkPos[actRoot].y)
+				{
+					rootPos = pos;
+					return;
+				}
 			}
 		}
 		else
@@ -424,16 +546,18 @@ void Enemy::Track(const GameCtrl & controller)
 				(*PosTbl[tmpAct][TBL_OPP]) += SpeedTbl[dirOpp[tmpAct][actRoot / 4]][0];
 				movePos += SpeedTbl[dirOpp[tmpAct][actRoot / 4]][0];
 			}
-			if ((*PosTbl[tmpAct][TBL_OPP]) == checkPos[actRoot].x)
+			if ((*PosTbl[tmpAct][TBL_OPP]) == plPos[nearP].x
+			||	(*PosTbl[tmpAct][TBL_OPP]) == checkPos[actRoot].x)
 			{
-				action = ENEM_ACT::SERCH;
+				action = ENEM_ACT::BACK;
 				return;
 			}
 		}
 		break;
 	case 1:
 	case 2:
-		if ((*PosTbl[tmpAct][TBL_MAIN]) != plPos[nearP].x)
+		if ((*PosTbl[tmpAct][TBL_MAIN]) != checkPos[actRoot].x
+			&& (*PosTbl[tmpAct][TBL_MAIN]) != plPos[nearP].x)
 		{
 			if (mapMoveTbl[static_cast<int>(lpMapCtrl.GetMapData(sidePos(pos, static_cast<DIR>(tmpAct), SpeedTbl[static_cast<DIR>(tmpAct)][0], -hitRad.x)))]
 				&& mapMoveTbl[static_cast<int>(lpMapCtrl.GetMapData(sidePos(pos, static_cast<DIR>(tmpAct), SpeedTbl[static_cast<DIR>(tmpAct)][0], hitRad.x - 1)))]
@@ -442,10 +566,12 @@ void Enemy::Track(const GameCtrl & controller)
 				dir = static_cast<DIR>(tmpAct);
 				(*PosTbl[tmpAct][TBL_MAIN]) += SpeedTbl[tmpAct][0];
 				movePos += SpeedTbl[tmpAct][0];
-			}
-			else
-			{
-				movePos += 0;
+				if ((*PosTbl[tmpAct][TBL_MAIN]) == plPos[nearP].x
+				|| (*PosTbl[tmpAct][TBL_MAIN]) == checkPos[actRoot].x)
+				{
+					rootPos = pos;
+					return;
+				}
 			}
 		}
 		else
@@ -458,18 +584,18 @@ void Enemy::Track(const GameCtrl & controller)
 				(*PosTbl[tmpAct][TBL_OPP]) += SpeedTbl[dirOpp[tmpAct][actRoot / 4]][0];
 				movePos += SpeedTbl[dirOpp[tmpAct][actRoot / 4]][0];
 			}
-			if ((*PosTbl[tmpAct][TBL_OPP]) == plPos[nearP].y)
+			if ((*PosTbl[tmpAct][TBL_OPP]) == plPos[nearP].y
+			|| (*PosTbl[tmpAct][TBL_OPP]) == checkPos[actRoot].y)
 			{
-				action = ENEM_ACT::SERCH;
+				action = ENEM_ACT::BACK;
 				return;
 			}
 		}
 		break;
 	}
-	//---------移動をしていなかった場合再度方向を決める-------------
 	if (movePos == 0)
 	{
-		action = ENEM_ACT::MOVE;	
+		action = ENEM_ACT::BACK;
 		return;
 	}
 }
@@ -477,14 +603,11 @@ void Enemy::Track(const GameCtrl & controller)
 //-------------ﾊﾟｽ探索(移動ﾊﾟｽがない時のみ)-----------
 void Enemy::Serch(const GameCtrl & controller)
 {
-	auto &chipSize = lpStageMng.GetChipSize().x;
-	CheckFree();
-	//---------------第一分岐点の設定----------------
-	for (DIR tmp = DIR_DOWN; tmp < DIR::DIR_MAX; tmp = static_cast<DIR>(tmp + 1))
+	if (actCnt == 60)
 	{
-		if (mapMoveTbl[static_cast<int>(lpMapCtrl.GetMapData(sidePos(pos, tmp, SpeedTbl[tmp][0], -hitRad.x)))]
-			&& mapMoveTbl[static_cast<int>(lpMapCtrl.GetMapData(sidePos(pos, tmp, SpeedTbl[tmp][0], hitRad.x - 1)))]
-			&& mapMoveTbl[static_cast<int>(lpMapCtrl.GetMapData(sidePos(pos, tmp, SpeedTbl[tmp][0], 0)))])
+		CheckFree();
+		//---------------第一分岐点の設定----------------
+		for (DIR tmp = DIR_DOWN; tmp < DIR::DIR_MAX; tmp = static_cast<DIR>(tmp + 1))
 		{
 			VECTOR2 tmpPos = pos;
 			checkPos[static_cast<int>(tmp)] = Distance(tmp, tmpPos);
@@ -492,9 +615,9 @@ void Enemy::Serch(const GameCtrl & controller)
 			//------------第二分岐点の設定---------------
 			for (int opp = 1; opp < 3; opp++)
 			{
-				DIR setDir = static_cast<DIR>(tmp + opp * 4);
-				checkPos[static_cast<int>(setDir)] = Distance(dirOpp[tmp][opp], checkPos[static_cast<int>(tmp)]);
-				passFlag[static_cast<int>(setDir)] = true;
+				int setDir = tmp + opp * 4;
+				checkPos[setDir] = Distance(dirOpp[tmp][opp], checkPos[static_cast<int>(tmp)]);
+				passFlag[setDir] = true;
 			}
 		}
 		//---------優先度を設定-------------
@@ -503,31 +626,27 @@ void Enemy::Serch(const GameCtrl & controller)
 			if (passFlag[e])
 			{
 				VECTOR2 exPos = { plPos[nearP].x - checkPos[e].x ,plPos[nearP].y - checkPos[e].y };
-				comPos[e] = static_cast<int>(sqrt(exPos.x*exPos.x) + sqrt(exPos.y * exPos.y));
+				comPos[e] = sqrt(exPos.x*exPos.x) + sqrt(exPos.y * exPos.y);
 			}
 		}
 		// 第一、第二分岐点までのDIRを決定
 		for (int e = 1; e < 12; e++)
 		{
-			if (comPos[e] < comPos[actNum[2]])
+			if (comPos[e] < comPos[actRoot])
 			{
 				actNum[2] = e;
 				actRoot = e;
 			}
 		}
-		if (actCnt <= 0)
-		{
-			action = ENEM_ACT::TRA;
-			actCnt = 60;
-			return;
-		}
-		actCnt--;
+	}
+	if (actCnt <= 0)
+	{
+		action = ENEM_ACT::TRA;
+		actCnt = 60;
 		return;
 	}
-}
-
-void Enemy::SpMove(const GameCtrl & controller)
-{
+	actCnt--;
+	return;
 }
 
 void Enemy::Escape(const GameCtrl & controller)
@@ -536,6 +655,66 @@ void Enemy::Escape(const GameCtrl & controller)
 
 void Enemy::Wait(const GameCtrl & controller)
 {
+	// 何もしない
+	return;
+}
+
+void Enemy::Back(const GameCtrl & controller)
+{
+	if (pos == firstPos)
+	{
+		action = ENEM_ACT::WAIT;
+		return;
+	}
+	int tmpAct = actRoot % 4;
+	int tmpAfter = actRoot / 4;
+	switch (rootPass[tmpAct][tmpAfter])
+	{
+	case 0:
+	case 3:
+		// 曲がったところへ移動
+		if ((*PosTbl[rootPass[tmpAct][tmpAfter]][TBL_MAIN]) != rootPos.y)
+		{
+			Enemy::dir = rootPass[tmpAct][tmpAfter];
+			(*PosTbl[rootPass[tmpAct][tmpAfter]][TBL_MAIN]) += SpeedTbl[Enemy::dir][0];
+		}
+		// 追跡開始地点へ移動
+		else if ((*PosTbl[rootPass[tmpAct][0]][TBL_MAIN]) != firstPos.x)
+		{
+			Enemy::dir = rootPass[tmpAct][0];
+			(*PosTbl[rootPass[tmpAct][0]][TBL_MAIN]) += SpeedTbl[Enemy::dir][0];
+		}
+		else
+		{
+			rootPos = VECTOR2(-1, -1);
+			firstPos = VECTOR2(-1, -1);
+			action = ENEM_ACT::CONST_MOVE;
+			return;
+		}
+		break;
+	case 1:
+	case 2:
+		// 曲がったところへ移動
+		if ((*PosTbl[rootPass[tmpAct][tmpAfter]][TBL_MAIN]) != rootPos.x)
+		{
+			Enemy::dir = rootPass[tmpAct][tmpAfter];
+			(*PosTbl[rootPass[tmpAct][tmpAfter]][TBL_MAIN]) += SpeedTbl[Enemy::dir][0];
+		}
+		// 追跡開始地点へ移動
+		else if ((*PosTbl[rootPass[tmpAct][0]][TBL_MAIN]) != firstPos.y)
+		{
+			Enemy::dir = rootPass[tmpAct][0];
+			(*PosTbl[rootPass[tmpAct][0]][TBL_MAIN]) += SpeedTbl[Enemy::dir][0];
+		}
+		else
+		{
+			rootPos = VECTOR2(-1, -1);
+			firstPos = VECTOR2(-1, -1);
+			action = ENEM_ACT::CONST_MOVE;
+			return;
+		}
+		break;
+	}
 	return;
 }
 
--- zelda/source/Enemy.h
+++ zelda/source/Enemy.h
@@ -27,10 +27,12 @@ enum class ENEMY_STATE
 // enemyの行動
 enum class ENEM_ACT
 {
-	DO_NOTHING,	// 何もしない
+	WAIT,	// 何もしない
 	MOVE,				// 移動
 	TRA,					// 追跡
 	SERCH,				// 探索
+	CONST_MOVE,	// 一定の場所を徘徊
+	BACK,				// 持ち場に戻る
 	MAX
 };
 
@@ -39,16 +41,17 @@ constexpr auto ENEMY_DASH_SPEED = 6;
 constexpr auto ENEMY_LIM = 201;
 constexpr auto PLAYER_DIS_X = 9;
 constexpr auto PLAYER_DIS_Y = 5;
+constexpr auto SERCH_LIM = 400;
 
 using PRIORITY_ARRAY = std::array<int, static_cast<int>(ENEM_ACT::MAX)>;
 using PRIORITY_TBL_ARRAY = std::array<PRIORITY_ARRAY, static_cast<int>(ENEMY_STATE::MAX)>;
 using PASS_FLAG = std::array<bool, DIR_MAX * 3>;
 using PASS_ARR_ALL = std::array<VECTOR2, DIR_MAX * 3>;
 using DIR_PASS = std::array<DIR[3], DIR_MAX>;
-using CHECK_ARR = std::vector<int>;
+using CHECK_ARR = std::vector<double>;
 
-using  OBJ_POS = std::array<VECTOR2, 4>;
-using  OBJ_INT = std::array<int, 4>;
+using OBJ_POS = std::array<VECTOR2, 4>;
+using OBJ_INT = std::array<int, 4>;
 
 class Enemy :
 	public Obj
@@ -63,10 +66,10 @@ public:
 	);
 	~Enemy();
 
-	bool CheckDeath(void);					// 死亡判定,死んでいなければfalseを返す
-	bool CheckObjType(OBJ_TYPE type);		// 指定した値と同じ型が来たらtrueを返す
-	const OBJ_TYPE& GetObjType(void)const;
-	void SetDeathFlag(bool deathFlag);
+	bool CheckDeath(void);								// 死亡判定,死んでいなければfalseを返す
+	bool CheckObjType(OBJ_TYPE type);			// 指定した値と同じ型が来たらtrueを返す
+	const OBJ_TYPE& GetObjType(void)const;	// ｵﾌﾞｼﾞｪｸﾄ名を返す
+	void SetDeathFlag(bool deathFlag);				// 死んでいればtrueをｾｯﾄする
 private:
 	// ﾌﾚｰﾑごとに実行するもの
 	void SetMove(const GameCtrl &controller, weakListObj objList);
@@ -80,49 +83,57 @@ private:
 	// 仮想ﾊﾟｽの探索
 	VECTOR2 Distance(DIR tmpDir,VECTOR2 pos);
 	// 近いﾌﾟﾚｲﾔｰの検索
-	int SerchPlayer(void);
+	int SerchPlayer(bool flag);
 
 	// ---------- ｴﾈﾐｰの状態関数 ------------
 	void Move(const GameCtrl & controller);			// ﾗﾝﾀﾞﾑ
-	void Track(const GameCtrl & controller);		// 追跡
-	void Serch(const GameCtrl & controller);		// 探索
-	void SpMove(const GameCtrl & controller);		// 指定行動
+	void Track(const GameCtrl & controller);			// 追跡
+	void Serch(const GameCtrl & controller);			// 探索
+	void ConstMove(const GameCtrl & controller);	// 指定行動
 	void Damage(const GameCtrl & controller);		// ﾀﾞﾒｰｼﾞ時
 	void Escape(const GameCtrl & controller);		// 逃げる
-	void Wait(const GameCtrl & controller);			// 待機ﾄ
+	void Wait(const GameCtrl & controller);			// 待機
+	void Back(const GameCtrl & controller);			// 帰宅
+
+	//--------------敵の種類ごと-----------------
+	void GateKeeper(void);		// 門番
+	void Undette(void);			// ｱﾝﾃﾞｯﾄ
+	void Zombie(void);				// ｿﾞﾝﾋﾞ
 
 	ENEMY_STATE state;			// 状態
 	ENEMY name;					// 敵の種類
-	ENEM_ACT action;			// 敵の行動
+	ENEM_ACT action;				// 敵の行動
 	// ------------------近いﾌﾟﾚｲﾔｰの検索用--------------------------
 	OBJ_POS plPos;				// ﾌﾟﾚｲﾔｰの座標
 	OBJ_INT enPos;				// 敵の座標
-	int nearP;					// 近いﾌﾟﾚｲﾔｰ
+	VECTOR2 firstPos;			// 初期座用
+	int nearP;						// 近いﾌﾟﾚｲﾔｰ
 
-	int faintCnt;				// 怯みｶｳﾝﾄ
 	int timeCnt;				// 経過時間のｶｳﾝﾄ
 	bool oppFlag;				// 移動反転ﾌﾗｸﾞ
 	int behaviorCnt;			// 行動時のｶｳﾝﾄ
 	int enCnt;					// 自分自身のﾅﾝﾊﾞｰ
 	int speed;					// 敵の移動速度
-	int actNum[3];				// 行動予定のﾙｰﾄ
+	int actNum[3];			// 行動予定のﾙｰﾄ
 	int actRoot;				// 実行するﾙｰﾄ
 	int movePos;				// 移動量	
 	int actCnt;					// 発見時の待ちﾌﾚｰﾑ
 	int deathCnt;				// 点滅用のｶｳﾝﾄ
 
-	CHECK_ARR comPos;		// 優先度の比較用配列(昇順)
-	CHECK_ARR comNum;		// 優先度の配列
-	VECTOR2 addCnt;			// 敵の移動量管理
-	DIR dir;				// 敵の向き
-	PASS_FLAG passFlag;		// 移動できるﾊﾟｽ
-	int comFlag;			// 優先配列に格納されている数
+	CHECK_ARR comPos;			// 優先度の比較用配列(昇順)
+	VECTOR2 addCnt;				// 敵の移動量管理
+	DIR dir;								// 敵の向き
+	PASS_FLAG passFlag;			// 移動できるﾊﾟｽ
+	int comFlag;						// 優先配列に格納されている数
 	PASS_ARR_ALL checkPos;	// 分岐点
-	DIR_PASS dirOpp;		// 垂直方向
+	DIR_PASS dirOpp;				// 垂直方向
 
-	DIR_TBL_ARY keyIdTbl;		// 移動方向
-	DIR_TBL_PTR PosTbl;			// ﾎﾟｲﾝﾀを直接格納
-	DIR_TBL_A2D SpeedTbl;		// 移動速度
+	VECTOR2 rootPos;			// 目標点
+	DIR_PASS rootPass;		// 帰宅方向
+
+	DIR_TBL_ARY keyIdTbl;				// 移動方向
+	DIR_TBL_PTR PosTbl;					// ﾎﾟｲﾝﾀを直接格納
+	DIR_TBL_A2D SpeedTbl;			// 移動速度
 	MAP_MOVE_TBL mapMoveTbl;	// 移動制御,移動可能ｵﾌﾞｼﾞｪｸﾄならtrueを返す←ｱｲﾃﾑや障害物を追加したときに使う
 	void (Enemy::*_updater)(const GameCtrl & controller);		// 状態関数ﾎﾟｲﾝﾀ
 protected:
